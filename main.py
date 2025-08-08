f241366ce7d3c149f57e5d384e65aa4d696cb7f5
GrayHat <mahmoudhussien2692004@gmail.com>
2025-08-08 21:36:36 +0200

    Final Project

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
                                    # ->>> Steps we have to pass through it to achieve our goal.
 
-#as an initialization we have to read words.txt file to make it as our dataset and determine a random word from it. //ok
-#first step, we need to implement [6][5] grid to make the user interface with it and guesses 6 words from Dataset every word contains 5 letters. //ok
-#The second step the user has 6 tries to guess a word that will be randomly selected from the game (code). //ok
-#We have 2 options first one that the user guessed a word not included in Dataset,so we have to set an error message that WORD NOT FOUND. //ok
-#second one that the user guessed the correct word from dataset so we have to determine letters that included in random word chose by game by its index. ->> {Bonus according to color (Green ->> correct index,Yellow ->> close index,Gray ->> letter no included)}. //ok
-#Game ends when user has guessed the word correctly before 6 tries or the user has finished his 6 tries or (additional property) ->> (user input is giveup). //ok
-#Score increase by 1 if user guessed the word correctly and vice versa it decreased by 1 if user failed to guess the word. //ok
-
- #بسم الله
+#as an initialization we have to read words.txt file to make it as our dataset and determine a random word from it.
+#first step, we need to implement [6][5] grid to make the user interface with it and guesses 6 words from Dataset every word contains 5 letters. 
+#The second step the user has 6 tries to guess a word that will be randomly selected from the game (code). 
+#We have 2 options first one that the user guessed a word not included in Dataset,so we have to set an error message that WORD NOT FOUND. 
+#second one that the user guessed the correct word from dataset so we have to determine letters that included in random word chose by game by its index. ->> {Bonus according to color (Green ->> correct index,Yellow ->> close index,Gray ->> letter no included)}. 
+#Game ends when user has guessed the word correctly before 6 tries or the user has finished his 6 tries or (additional property) ->> (user input is giveup). 
+#Score increase by 1 if user guessed the word correctly and vice versa it decreased by 1 if user failed to guess the word. 
+                                                             #بسم الله
 import random
                                                              # ->>>Initialization.
 
@@ -18,8 +17,8 @@ file_words1 = Initialization.readlines() # return a list, every item on it is li
 file_words2 = [x[:-1] for x in file_words1] # we have a built-in functions to solve this problem but i prefer SLICING to solve it, this technique loops inside every item in the list and kills the last character on it which is \n character.
 # print(file_words2) # for Debbuging
 
-                                               # ->>>First and Second steps.
-                                                # ->>>Ending the game and the score counter.
+                                                             # ->>>First and Second steps.
+                                                             # ->>>Ending the game and the score counter.
 
 
 count = 0
@@ -55,6 +54,7 @@ while 1:# starting the game and make it continuous until the user cutoff the inf
                         print(f"{guesses[y]} is a valid letter in the Random_word") # second possible hint to the user
                 else:
                         print(f"{guesses[y]} is not a valid letter in the Random_word") # third possible hint to the user
+                  
     elif guesses == "giveup": # Break loop to end the game when the user gives up.
         print("YOU LOST!")
         print(f"The secret word is {Random_word}")
@@ -62,17 +62,18 @@ while 1:# starting the game and make it continuous until the user cutoff the inf
         print(f"Your score is {count}")
         Status = True
         break
+      
     elif guesses not in file_words2:
         print("WORD NOT VALID, Try again!") # The word not valid (not in the dataset)
         i -= 1 # I used this technique to repeat the iteration in which the user input was invalid word (not from the dataset)
     i += 1 # Increase the increment by one every iteration
 
-  
   if Status == False:
    print("YOU LOST!") # failed to guess the Random_word
    print(f"The secret word is {Random_word}")
    count -= 1 # Counter that indicates the score
    print(f"Your score is {count}")  
+
 
 
 
