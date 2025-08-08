2a4cf462189f782a6f73198e214ae48a72fb7ac9
GrayHat <mahmoudhussien2692004@gmail.com>
2025-08-08 21:36:29 +0300

    Steps we must pass through it to achieve our goal.

---
+++ main.py
@@ -0,0 +1,19 @@
+                                   # ->>> Steps we have to pass through it to achieve our goal.
+
+#as an initialization we have to read words.txt file to make it as our dataset and determine a random word from it. //ok
+#first step, we need to implement [6][5] grid to make the user interface with it and guesses 6 words from Dataset every word contains 5 letters. //ok
+#The second step the user has 6 tries to guess a word that will be randomly selected from the game (code). //ok
+#We have 2 options first one that the user guessed a word not included in Dataset,so we have to set an error message that WORD NOT FOUND. //ok
+#second one that the user guessed the correct word from dataset so we have to determine letters that included in random word chose by game by its index. ->> {Bonus according to color (Green ->> correct index,Yellow ->> close index,Gray ->> letter no included)}. //ok
+#Game ends when user has guessed the word correctly before 6 tries or the user has finished his 6 tries or (additional property) ->> (user input is giveup). //ok
+#Score increase by 1 if user guessed the word correctly and vice versa it decreased by 1 if user failed to guess the word. //ok
+
+
+
+
+
+
+
+
+
+
